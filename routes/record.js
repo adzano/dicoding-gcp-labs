5c274c046e9899e9f7aa6659fe96b10ebc798159
XYLO <62704521+adzano@users.noreply.github.com>
2023-05-09 23:00:34 +0700

    Update record.js

--- routes/record.js
+++ routes/record.js
@@ -11,10 +11,10 @@ const multer = Multer({
 
 // TODO: Sesuaikan konfigurasi database
 const connection = mysql.createConnection({
-    host: 'public_ip_sql_instance_Anda',
+    host: '35.188.165.35',
     user: 'root',
     database: 'nama_database_Anda',
-    password: 'password_sql_Anda'
+    password: 'zanzanzan111'
 })
 
 router.get("/dashboard", (req, res) => {
